75b9d9eefb8d6878edfe3b9bcce7785565702a7e
Khushbu Dharmendra Patle <PRIYA.SURESH1198@GMAIL.COM>
2025-07-28 20:10:56 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-  API_BASE_URL: "http://localhost:8080"
+  API_BASE_URL: "http://13.234.56.78:8080"  // backend EC2 IP and backend port
 };
 
 export default config;
