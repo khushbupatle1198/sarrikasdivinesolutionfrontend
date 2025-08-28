e8eacabfecebfcc38f4ac3317458e758ff4be413
Khushbu Dharmendra Patle <PRIYA.SURESH1198@GMAIL.COM>
2025-08-28 14:30:46 +0530

    Update Consultation.js
    remove axios

--- src/components/Consultation.js
+++ src/components/Consultation.js
@@ -5,9 +5,6 @@ import Footer from './Footer';
 import WhatsAppToggle from './WhatsAppToggle';
 import './Consultation.css';
 import config from "../config";
-import axios from "axios";
-
-axios.post(`${config.API_BASE_URL}/api/savecourse`, { name: "React" });
 
 
 
